6f5e5d903a864525e0d5fa9303f850f3ffd312eb
Piyush Kadam <88472309+PiyushKadam07@users.noreply.github.com>
2021-09-02 15:05:27 +0530

    Delete employee_payroll directory

--- employee_payroll/employee_payroll/js/filter.js
+++
@@ -1,9 +0,0 @@
-function w3_open() {
-    var x = document.getElementById("filter");
-    if ( x.style.display === 'none') {
-        x.style.display = 'block'
-    }
-    else {
-        x.style.display = 'none'
-    }
-}
